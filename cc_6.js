3dc898a088a85161efcdf1626f3bffce349af32e
Isabelalx <isabelalopesxavier@usf.edu>
2025-02-09 21:11:17 -0600

    Task 1 - Business Profit Calculation.

---
+++ cc_6.js
@@ -0,0 +1,9 @@
+// task 1
+
+// Business Profit Calculation
+function calculateProfit(costPrice, sellingPrice, unitsSold) {
+    let profit = (sellingPrice - costPrice) * unitsSold;
+    return `Total Profit: $${profit}`;
+}
+console.log(calculateProfit(20, 30, 100)); // "Total Profit: $1000"
+console.log(calculateProfit(50, 70, 200)); // "Total Profit: $4000"
