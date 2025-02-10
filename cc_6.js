df462e1564b6ac482bc4c29295bb3df05b7bf768
Isabelalx <isabelalopesxavier@usf.edu>
2025-02-09 21:13:02 -0600

    Task 3 - Employee Bonus Calculation.

--- cc_6.js
+++ cc_6.js
@@ -17,3 +17,14 @@ const calculateSalesTax = function(amount, taxRate) {
 console.log(calculateSalesTax(100, 0.07)); // "Sales Tax: $7"
 console.log(calculateSalesTax(500, 0.1));  // "Sales Tax: $50"
 
+// task 3
+
+// Employee Bonus Calculation
+const calculateBonus = (salary, performanceRating) => {
+    let bonusRate = performanceRating === "Excellent" ? 0.2 :
+                    performanceRating === "Good" ? 0.1 :
+                    performanceRating === "Average" ? 0.05 : 0;
+    return `Bonus: $${salary * bonusRate}`;
+};
+console.log(calculateBonus(5000, "Excellent")); // "Bonus: $1000"
+console.log(calculateBonus(7000, "Good"));      // "Bonus: $700"
